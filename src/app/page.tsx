ef3337e59955b72b62ceadfa4e373a80460f8ced
chucoding <chucoding@gmail.com>
2023-02-14 00:02:24 +0900

    feat : gradient background

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
-    <main>
+    <main className="bg-gradient-to-b from-indigo-500 via-purple-500 to-pink-500">
       <h1 className="text-3xl font-bold underline">Hello world!</h1>
     </main>
   );
