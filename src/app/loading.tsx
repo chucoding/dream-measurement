89737ad8efbabca506c7107a5fb005054974a51e
chucoding <chucoding@gmail.com>
2023-02-17 00:10:51 +0900

    fix : stack overflow

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,12 +1,12 @@
 import { Suspense } from "react";
 import Layout from "./layout";
-import Home from "./page";
+import Main from "./page";
 
 export default function Loading() {
   return (
     <Layout>
-      <Suspense fallback={<Loading />}>
-        <Home />
+      <Suspense fallback={<p>Loading...</p>}>
+        <Main />
       </Suspense>
     </Layout>
   );
