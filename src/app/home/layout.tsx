dd4675278cf5af17f6cd85c4f245359cefbe054f
chucoding <chucoding@gmail.com>
2023-02-17 00:11:16 +0900

    feat : create app layout

---
+++ src/app/home/layout.tsx
@@ -0,0 +1,9 @@
+export default function App() {
+  return (
+    <div>
+      <button className="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">
+        테스트 시작
+      </button>
+    </div>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
 import { Inter } from "@next/font/google";
+import App from "./home/layout";
 
 const inter = Inter({ subsets: ["latin"] });
 
-export default function Home() {
+export default function Main() {
   return (
-    <main className="h-screen bg-gradient-to-b from-[#84A1B1] via-[#9C7E76] to-[#6C5873]"></main>
+    <main className="h-screen bg-gradient-to-b from-[#84A1B1] via-[#9C7E76] to-[#6C5873]">
+      <App />
+    </main>
   );
 }
