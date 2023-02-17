7e928b339f2c118898a4b3cb589387dc02caeba9
chucoding <chucoding@gmail.com>
2023-02-18 00:18:19 +0900

    feat : font

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,16 @@
+const { fontFamily } = require("tailwindcss/defaultTheme");
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./app/**/*.{js,ts,jsx,tsx}",
-    "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
-
-    // Or if using `src` directory:
-    "./src/**/*.{js,ts,jsx,tsx}",
+    "./src/app/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
+      fontFamily: {
+        sans: ["var(--font-inter)", ...fontFamily.sans],
+      },
       screens: {
         tall: { raw: "(min-height: 800px)" },
       },
