1bec6cf3d9a8a226c36186979621d3ac4d28f9fc
chucoding <chucoding@gmail.com>
2023-03-07 01:50:24 +0900

    feat : cloudCardWrapper

--- src/components/templates/home.tsx
+++ src/components/templates/home.tsx
@@ -1,31 +1,34 @@
 "use client";
 import { useState } from "react";
+import CloudCardWrapper from "../modules/cloudCardWrapper";
 import OnBoarding from "./onboarding";
 
 export default function App(props: any) {
   const [start, setStart] = useState(false);
-
   return start ? (
     <OnBoarding />
   ) : (
-    <div>
+    <>
       <div className="inline-block text-center">
-        <img className="absolute right-2" src="/cloud.png" alt="메인이미지" />
-        <h1 className="text-2xl absolute top-32 left-16">꿈력 측정 테스트</h1>
-        <h2 className="text-1xl absolute top-40" style={{ left: "88px" }}>
-          당신의 꿈력은 몇 %?
-        </h2>
+        <CloudCardWrapper>
+          <div className="absolute top-[40%] left-[30%]">
+            <h1 className="text-5xl font-black">꿈력 측정 테스트</h1>
+            <h2 className="text-2xl absolute mt-2 left-[30%]">
+              당신의 꿈력은 몇 %?
+            </h2>
+          </div>
+        </CloudCardWrapper>
       </div>
-      <button
-        className="relative top-80 bg-white hover:bg-[#808080] text-black font-bold py-2 px-12 rounded-full"
-        onClick={() => setStart(true)}
-      >
-        테스트 시작하기
-        <br />
-      </button>
-      <p className="relative top-80 mt-2 text-gray-100 text-xs">
-        모바일 환경을 추천합니다.
-      </p>
-    </div>
+      <div className="relative top-[70%]">
+        <button
+          className="bg-white hover:bg-[#808080] text-black font-bold py-2 px-12 rounded-full"
+          onClick={() => setStart(true)}
+        >
+          테스트 시작하기
+          <br />
+        </button>
+        <p className=" mt-2 text-gray-100 text-xs">모바일 환경을 추천합니다.</p>
+      </div>
+    </>
   );
 }
