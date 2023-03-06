e6f06781867d9d04998f9a030b16557a227ccf74
chucoding <chucoding@gmail.com>
2023-03-07 02:32:35 +0900

    design : iPhone12 base

--- src/components/modules/cloudCardWrapper.tsx
+++ src/components/modules/cloudCardWrapper.tsx
@@ -6,8 +6,8 @@ export default function CloudCardWrapper({
   return (
     <div
       className={`bg-[url('/cloud.png')] bg-cover
-        h-[400px] w-[450px]
-        absolute left-[40%] transform -translate-x-1/2 top-[40%] -translate-y-1/2
+        h-[400px] w-[420px]
+        absolute left-[43%] transform -translate-x-1/2 top-[40%] -translate-y-1/2
         ${runAnimate ? "animate-fadein" : ""}`}
     >
       {children}
--- src/components/templates/home.tsx
+++ src/components/templates/home.tsx
@@ -11,9 +11,9 @@ export default function App(props: any) {
     <>
       <div className="inline-block text-center">
         <CloudCardWrapper>
-          <div className="absolute top-[40%] left-[30%]">
+          <div className="absolute top-[35%] left-[27%]">
             <h1 className="text-5xl font-black">꿈력 측정 테스트</h1>
-            <h2 className="text-2xl absolute mt-2 left-[30%]">
+            <h2 className="text-2xl font-medium absolute mt-2 left-[23%]">
               당신의 꿈력은 몇 %?
             </h2>
           </div>
@@ -22,12 +22,15 @@ export default function App(props: any) {
       <div className="relative top-[70%]">
         <button
           className="bg-white hover:bg-[#808080] text-black font-bold py-2 px-12 rounded-full"
+          style={{ height: "57px", width: "200px", fontSize: "21px" }}
           onClick={() => setStart(true)}
         >
           테스트 시작하기
           <br />
         </button>
-        <p className=" mt-2 text-gray-100 text-xs">모바일 환경을 추천합니다.</p>
+        <p className=" mt-2 text-gray-100" style={{ fontSize: "18px" }}>
+          모바일 환경을 추천합니다.
+        </p>
       </div>
     </>
   );
