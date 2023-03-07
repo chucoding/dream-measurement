1557d4b8e28154c633236047badacd6fa5e4cd53
chucoding <chucoding@gmail.com>
2023-03-07 23:36:25 +0900

    design : onboarding

--- src/components/modules/cloudCardBig.tsx
+++ src/components/modules/cloudCardBig.tsx
@@ -3,7 +3,7 @@ export default function CloudCardBig({ children, runAnimate = false }: any) {
   return (
     <div
       className={`bg-[url('/cloud.png')] bg-cover
-        h-[400px] w-[450px]
+        h-[400px] w-[450px] p-10 pt-20 pb-20
         absolute left-[40%] transform -translate-x-1/2 top-[40%] -translate-y-1/2
         ${runAnimate ? "animate-fadein" : ""}`}
     >
