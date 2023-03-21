9d9ae68f3a7abd45f2be971c72b4cded61ea9fe6
chucoding <chucoding@gmail.com>
2023-03-21 23:55:12 +0900

    fix : render issue in ads

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,12 @@ import "./globals.css";
 import Head from "./head";
 import Script from "next/script";
 
+declare global {
+  interface Window {
+    adsbygoogle: any;
+  }
+}
+
 export default function RootLayout({
   children,
 }: {
@@ -33,24 +39,6 @@ export default function RootLayout({
           <div className="text-center w-[375px] h-full relative">
             <div className="w-full h-full absolute top-0 bottom-0 align-middle">
               {children}
-            </div>
-            <div style={{ textAlign: "center" }}>
-              <Script
-                async
-                src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1919598055512436"
-                crossOrigin="anonymous"
-              ></Script>
-              <ins
-                className="adsbygoogle"
-                style={{
-                  display: "inline-block",
-                  width: "120px",
-                  height: "50px",
-                }}
-                data-ad-client="ca-pub-1919598055512436"
-                data-ad-slot="9317183248"
-              ></ins>
-              <Script id="adsbygoogle">{`(adsbygoogle = window.adsbygoogle || []).push({});`}</Script>
             </div>
           </div>
         </main>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,35 @@
 "use client";
 import App from "../components/templates/home";
+import { useEffect } from "react";
+import Script from "next/script";
 
 export default function Main() {
-  return <App />;
+  useEffect(() => {
+    try {
+      (window.adsbygoogle = window.adsbygoogle || []).push({});
+    } catch (e) {}
+  }, []);
+
+  return (
+    <>
+      <App />
+      <div style={{ textAlign: "center" }}>
+        <Script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1919598055512436"
+          crossOrigin="anonymous"
+        ></Script>
+        <ins
+          className="adsbygoogle"
+          style={{
+            display: "inline-block",
+            width: "120px",
+            height: "50px",
+          }}
+          data-ad-client="ca-pub-1919598055512436"
+          data-ad-slot="9317183248"
+        ></ins>
+      </div>
+    </>
+  );
 }
