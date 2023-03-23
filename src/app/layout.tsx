b7f483d5657aacf482d48e2593d7e3efb25e5d60
chucoding <chucoding@gmail.com>
2023-03-23 22:34:12 +0900

    feat : remove ads (: not necessary)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,34 +2,16 @@
 
 import "./globals.css";
 import Head from "./head";
-import Script from "next/script";
-import { useEffect } from "react";
-
-declare global {
-  interface Window {
-    adsbygoogle: any;
-  }
-}
 
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  useEffect(() => {
-    try {
-      (window.adsbygoogle = window.adsbygoogle || []).push({});
-    } catch (e) {}
-  }, []);
-
   return (
     <html lang="ko" style={{ fontFamily: "GangwonEdu_OTFBoldA" }}>
       <head>
         <Head />
-        <Script
-          defer
-          src="https://cdn.swygbro.com/public/widget/swyg-widget.js"
-        ></Script>
       </head>
       <body>
         <main
@@ -47,23 +29,6 @@ export default function RootLayout({
         >
           <div className="text-center w-[375px] h-full relative">
             <div className="w-full h-full absolute top-0 bottom-0 align-middle">
-              <div className="absolute top-0">
-                <Script
-                  async
-                  src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1919598055512436"
-                  crossOrigin="anonymous"
-                ></Script>
-                <ins
-                  className="adsbygoogle"
-                  style={{
-                    display: "inline-block",
-                    width: "320px",
-                    height: "100px",
-                  }}
-                  data-ad-client="ca-pub-1919598055512436"
-                  data-ad-slot="2044520891"
-                ></ins>
-              </div>
               {children}
             </div>
           </div>
